614dac195a029456ccb7cd8ca18e9e814f58aa7f
humphrey <humphreyguohd@gmail.com>
2022-03-26 17:09:03 +1100

    F

--- sample-bot.py
+++ sample-bot.py
@@ -155,7 +155,7 @@ def main():
                             exchange.send_add_message(order_id=orderId, symbol="VALE", dir=Dir.BUY, price=valeSell, size=valeSize)
                             sellOrders.append({"type":"add", "order_id": orderId, "symbol": "VALBZ", "dir": "BUY", "price": valbz_bid_price, "size": valeSize})
                             orderId += 1
-                            ownedVale = 10
+                            ownedVale += valeSize
                             print(f"Buying {valeSize} Vale for {valeSell}")
                         # if valbz_bid_price > valeSell and valeSize <= 10:
                         #     exchange.send_add_message(order_id=orderId, symbol="VALE", dir=Dir.BUY, price=valeSell, size=valeSize)
